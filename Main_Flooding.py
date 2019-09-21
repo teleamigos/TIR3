bd40ea333dea08880fd4a4b932be1c8831e9fb96
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-09-21 18:20:35 -0500

    make threads

--- Main_Flooding.py
+++ Main_Flooding.py
@@ -1,26 +1,35 @@
-from Flooding import *
-#from socket import socket, AF_PACKET, SOCK_RAW, htons
-
 """-----------------------------------------------------------------------------
 ---------------------------This is main file------------------------------------
 -----------------------------------------------------------------------------"""
+from socket import socket, AF_PACKET, SOCK_RAW, htons
+from Flooding import*
+from Threaded_send import*
+import threading
+from _thread import *
 """-----------------------------------------------------------------------------
 ----------------------------Init section-------------------------------------"""
-F=Flooding('A')
-#s=socket(AF_PACKET,SOCK_RAW,htons(0x0800))
-#s.bind(("wlp1s0",0))
+usr_name=input("Type your user name : ")
+F=Flooding(usr_name)
+s=socket(AF_PACKET,SOCK_RAW,htons(0x0800))
+s.bind(("wlp1s0",0))
 """-----------------------------------------------------------------------------
-----------------------------User section-------------------------------------"""
-print("You are user A, your direction is : ", F.user['A'])
-op=input("Type Y fo send a message : ")
-if op=="Y":
-    """ getting information """
-    user =input("Type the user you want send a message(B,C or D) : ")
-    message=input("Type a message for user {user} :")
-    F.create_payload(0,message)
-    msj_out=F.create_package('A','D')
-    """-------------------------------------------------------------------------
-    ------------------------send information---------------------------------"""
-else :
-    """Listening for somenthin...."""
-    pass
+--------------------------------User-----------------------------------------"""
+op=input("Type Y/n to send a message ")
+while True:
+    if op=='Y':
+        dst_user=input("Type the user you want to send something : " )
+        message=input("Type a message you want to send to {} :".format{dst_user})
+        F.create_payload(0,message)
+        msj_out=F.create_package(usr_name,dst_user)
+        #Abrimos nuevo hilo para enviar
+    msj=s.recv(1024)
+    if not data:
+        print("Not infomation...")
+        break
+    if F.Unpack_message(msj)!='0':
+        msj_rcved=F.Unpack_message(msj)
+        print("Message received from {} :".format(F.src_add),msj_rcved)
+        op=input("Do you want to reply ? Y/N :")
+    else :
+        #Abrimos nuevo hilo para retransmitir
+        pass
