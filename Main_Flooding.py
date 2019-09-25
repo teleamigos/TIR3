201a51813362832abac38f3f18f94319f2ed6819
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-09-24 23:16:39 -0500

    It's ready for test.

--- Main_Flooding.py
+++ Main_Flooding.py
@@ -11,17 +11,17 @@ from threading import Thread
 ----------------------------Init section-------------------------------------"""
 usr_name=input("Type your user name : ")
 F=Flooding(usr_name)
-#s=socket(AF_PACKET,SOCK_RAW,htons(0x0801))
-#s.bind(("wlp1s0",0))
+s=socket(AF_PACKET,SOCK_RAW,htons(0x0801))
+s.bind(("wlp1s0",0))
 print("Your user ID   : ",F.my_ID)
 c=0
 ID=0
 """-----------------------------------------------------------------------------
 --------------------------------User-----------------------------------------"""
+print("Connection established.")
 op=input('Enter "Y" to send a message : ')
 while True:
     if op=='Y':
-        print(F.sequence,ID,F.my_ID)
         if F.sequence==0:
             """First message"""
             dst_user=input("Type the user you want to send something (A,B,C or D) : " )
@@ -30,51 +30,52 @@ while True:
             print("payload : ",payload)
             msj_out=F.create_package(F.broadcast,F.my_add,payload)
             print("Message to ID {}  and sequence {} to send  : ".format(F.user[dst_user],F.sequence),msj_out)
-            #start=time.time()
-            #thread1=Thread(target=threaded_send,args=(msj_out,s))
-            #thread1.start()
-            #print("------------")
-            #thread1.join()
+            start=time.time()
+            thread1=Thread(target=threaded_send,args=(msj_out,s))
+            thread1.start()
+            thread1.join()
         elif ID==F.my_ID:
             """This is not a first message"""
             message=input("Type a message you want to send to {} with ID {} :".format(dst_user,F.user[dst_user]))
+            self.sequence +=1
             payload=F.create_payload(dst_user,message)
             print("payload : ",payload)
             msj_out=F.create_package(dst_add,F.my_add,payload)
             print("Message to ID {}  and sequence {} to send  : ".format(F.user[dst_user],F.sequence),msj_out)
-            #start=time.time()
-            #thread1=Thread(target=threaded_send,args=(msj_out,s))
-            #thread1.start()
-            #print("------------")
-            #thread1.join()
-
-    #msj=s.recv(1024)
-    """    if not msj:
-            print("Not infomation...")
-            break"""
-    if F.Unpack_message(msj_out)!='0':
+            start=time.time()
+            thread1=Thread(target=threaded_send,args=(msj_out,s))
+            thread1.start()
+            thread1.join()
+    print("Listening : ")
+    msj=s.recv(1024)
+    if not msj:
+        print("Not infomation received.")
+        break
+    if F.Iscorrect(msj_out)!='0':
         """Unpack de message"""
-        ID,message,dst_add=F.Unpack_message(msj_out)
-        print('Message received from ID : {}'.format(ID),message)
-        for u in F.user:
-            if F.user[u]==ID:
-                dst_user=u
-        op=input('Type "Y" to reply to the user {}'.format(dst_user) )
-        F.sequence +=1
-        if op !='Y':
-            F.sequence=0
-            ID=0
-            op='Y'
-        
+        if F.IsRepeated(msj_out)=='1':
+            ID,message,dst_add=F.Unpack_message(msj_out)
+            print('Message received from ID : {}'.format(ID),message)
+            for u in F.user:
+                if F.user[u]==ID:
+                    dst_user=u
+            op=input('Type "Y" to reply to the user {} : '.format(dst_user) )
+            #F.sequence +=1
+            if op !='Y':
+                F.sequence=0
+                ID=0
+                op='Y'
+        else:
+            print("Message was discarded...")
+
     else :
         """Retransmitting"""
         L=len(msj_out)
         payload=msj_out[14:L-4]
         new_message=F.retransmitting(msj_out,payload)
-        print("message retransmitted...",new_message)
-        #start=time.time()
-        #thread1=Thread(target=threaded_send,args=(msj_out,s))
-        #thread1.start()
-        #print("------------")
-        #thread1.join()
+        print("message is retransmitted...",new_message)
+        start=time.time()
+        thread1=Thread(target=threaded_send,args=(msj_out,s))
+        thread1.start()
+        thread1.join()
         F.sequence=0
--- Threaded_send.py
+++ Threaded_send.py
@@ -1,8 +1,10 @@
 """-----------------------------------------------------------------------------
 ---------------------------This is thread file----------------------------------
 -----------------------------------------------------------------------------"""
+import time
 def threaded_send(msj,s):
+    print("Your message is being sent.")
+    time.sleep(1)
     print("to send : ",msj)
     s.sendall(msj)
-    print("message sent...")
-    
+    print("Your message was sent.")
